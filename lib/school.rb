ea18275b6d61c3c4a8f5117187a545abc541a9c9
ahare14 <alexanderhare14@gmail.com>
2019-04-27 10:45:55 -0600

    completed lab

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
 # code here!
+
+require 'pry'
+
+class School
+  attr_accessor :roster
+
+  def initialize(school)
+  	@school = school
+  	@roster = {}
+  end
+
+  def add_student(name,grade)
+  	if @roster.has_key?(grade)
+  		@roster[grade] << name
+  	else
+  		@roster[grade] = [name]
+  	end
+  end
+
+  def grade(grade)
+  	@roster[grade]
+  end
+
+  def sort
+  	@roster.each do |key, value|
+  		value.sort!
+  	end
+  end  
+end
+
+
+
+# school_1 = School.new("sacred heart")
+
+# school_2 = School.new("hamilton")
+
+
+#   	binding.pry
+
+
